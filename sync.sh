76958097bae277f63a6ea77d8c180079177ad407
albertchan <albert1chan@foxmail.com>
2019-12-15 12:37:06 +0800

    sync from source

---
+++ sync.sh
@@ -0,0 +1,8 @@
+git branch
+git remote add upstream https://github.com/gothinkster/golang-gin-realworld-example-app.gi 
+git remote -v
+git fetch upstream
+
+git merge upstream/master
+git push origin master
+
